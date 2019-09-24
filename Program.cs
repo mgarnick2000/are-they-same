624fe41ace4d087a5d4aaa7d2e72483e6ffc9dcd
Mark <mgarnick@techtonicgroup.com>
2019-09-24 13:27:57 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace are_they_same
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
